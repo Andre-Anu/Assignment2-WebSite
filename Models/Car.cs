84962747b565e23c4d4c6a7d1e2f7d4d9aa01d47
Anu <141456738+Andre-Anu@users.noreply.github.com>
2024-04-16 12:52:49 -0400

    Add files via upload

---
+++ Models/Car.cs
@@ -0,0 +1,13 @@
+﻿namespace WebSite.Models
+{
+    public class Car
+    {
+        public int? Id { get; set; }
+        public String? Model { get; set; }
+        public String? Company { get; set; }
+        public int? Year { get; set; }
+        public String? Image { get; set; }
+
+        //We will Check
+    }
+}
---
+++ Pages/CarPage.cshtml.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebSite.Models;
+using WebSite.Services;
+
+namespace WebSite.Pages
+{
+    public class CarPageModel : PageModel
+    {
+        public CarService CarService;
+
+        public IEnumerable<Car>? Cars { get; set; }
+
+        public CarPageModel(CarService carService)
+        {
+            CarService = carService;
+        }
+        public void OnGet()
+        {
+            Cars = CarService.GetCars();
+        }
+    }//class
+}//namespace
